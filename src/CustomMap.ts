267bef1c11621cc5073403464377a5d5e4257533
David Landes <davidkailandes@gmail.com>
2022-09-13 16:58:35 -0400

    added Marker Content

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,12 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// Instructions to every other class on how
+// tehy can be an ARGUMENT to "addMarker"
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,7 +21,20 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {}
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
 
-  addCompanyMarker(company: Company): void {}
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
 }
