10789f9d4fae2abaacdc4d5973a72e21d213dd69
David Landes <davidkailandes@gmail.com>
2022-09-14 09:48:45 -0400

    added some more logic to customMap, user and company

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,11 +1,12 @@
 // Instructions to every other class on how
 // tehy can be an ARGUMENT to "addMarker"
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
   markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
